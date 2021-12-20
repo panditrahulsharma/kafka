1abf57bb415287c7b9e3afa0bfef6a1bc23b2c87
panditrahulsharma <rahulpandit151197@gmail.com>
2021-12-20 18:28:54 +0530

    ksql

--- ksql_forsk_project/ksql/statements.sql
+++ ksql_forsk_project/ksql/statements.sql
@@ -22,27 +22,13 @@ tasks
 raw_telecom >> replace_simple_with_standard_terminology >> combine_all_services
 """
 
-DROP STREAM raw_telecom ;
+DROP STREAM if exists raw_telecom ;
 SET 'auto.offset.reset' = 'earliest';
 -- create a raw strem
 CREATE STREAM  raw_telecom WITH \
 (KAFKA_TOPIC='dbserver2.inventory.raw_telecom', VALUE_FORMAT='AVRO');
--- CREATE STREAM raw_telecom (
---     f VARCHAR,
---     jh VARCHAR,
---     la VARCHAR
---   ) WITH (
---     KAFKA_TOPIC = 'dbserver2.inventory.raw_telecom',
---     VALUE_FORMAT = 'JSON'
---   );
 
--- 
--- create a stream replace_simple_with_standard_terminology
-
--- CREATE STREAM replace_simple_with_standard_terminology AS SELECT  AFTER -> A,AFTER -> JH ,AFTER -> LA   FROM RAW_TELECOM;
-
-
--- filter of F table
+-- filter of  tables
 SET 'auto.offset.reset' = 'earliest';
 CREATE STREAM replace_simple_with_standard_terminology AS 
 SELECT
@@ -87,49 +73,36 @@ https://docs.ksqldb.io/en/latest/how-to-guides/use-a-custom-timestamp-column/
 
 -- first step double to bigint
 -- change double to varchar
-DROP STREAM double_to_BIGINT_timestamp;
+DROP STREAM if exists double_to_BIGINT_timestamp;
 SET 'auto.offset.reset' = 'earliest';
 CREATE STREAM double_to_BIGINT_timestamp
   WITH(KAFKA_TOPIC='double_to_BIGINT_timestamp') AS
   SELECT 
-    CAST(AFTER->J AS BIGINT) as start_time
+    CAST(AFTER->J AS BIGINT) as start_time,
+    CAST(AFTER->N AS BIGINT) as end_time
   FROM raw_telecom
   EMIT CHANGES;
 
 -- 2nd step would be bigint to timestamp
 https://docs.ksqldb.io/en/latest/developer-guide/ksqldb-reference/scalar-functions/#exp
--- DROP STREAM BIGINT_to_timestamp;
--- SET 'auto.offset.reset' = 'earliest';
--- CREATE STREAM BIGINT_to_timestamp AS 
--- SELECT 
---        CAST(start_time AS VARCHAR) as CAST_date,
---        SUBSTRING(CAST(start_time AS VARCHAR),0,8) as date1,
---        SUBSTRING(CAST(start_time AS VARCHAR),9,15) as date2
--- FROM double_to_BIGINT_timestamp
--- EMIT CHANGES;
-DROP STREAM BIGINT_to_timestamp;
+
+DROP STREAM if exists BIGINT_to_timestamp;
 SET 'auto.offset.reset' = 'earliest';
 CREATE STREAM BIGINT_to_timestamp AS 
 SELECT 
        CAST(start_time AS VARCHAR) as CAST_date,
        SUBSTRING(CAST(start_time AS VARCHAR),0,8) as call_start_date,
-       SUBSTRING(CAST(start_time AS VARCHAR),9,LEN(CAST(start_time AS VARCHAR))) as call_start_time
+       SUBSTRING(CAST(start_time AS VARCHAR),9,LEN(CAST(start_time AS VARCHAR))) as call_start_time,
+       SUBSTRING(CAST(end_time AS VARCHAR),0,8) as call_end_date,
+       SUBSTRING(CAST(end_time AS VARCHAR),9,LEN(CAST(end_time AS VARCHAR))) as call_end_time
 FROM double_to_BIGINT_timestamp
 EMIT CHANGES;
--- -----3rd step--------start date---------------------
-DROP STREAM startdate;
+
+
+-- -----3rd step--------start date with week day name---------------------
+DROP STREAM if exists startdate;
 SET 'auto.offset.reset' = 'earliest';
 CREATE STREAM startdate AS 
-SELECT 
-  CONCAT_WS('-', SUBSTRING(call_start_date,0,4),SUBSTRING(call_start_date,5,2),SUBSTRING(call_start_date,7,2)) as start_call_date
-FROM BIGINT_to_timestamp
-EMIT CHANGES;
-
--- -----3rd step--------start time---------------------
--- ---------------------am to pm
-DROP STREAM starttime;
-SET 'auto.offset.reset' = 'earliest';
-CREATE STREAM starttime AS 
 SELECT 
   CASE 
     WHEN CAST(SUBSTRING(CALL_START_TIME,0,2) as int)>=12 THEN
@@ -145,16 +118,12 @@ SELECT
         CONCAT_WS(':', SUBSTRING(CALL_START_TIME,0,2),SUBSTRING(CALL_START_TIME,3,2),SUBSTRING(CALL_START_TIME,5,2))
       END  
   END as am_pm_time,
-  CONCAT_WS(':',SUBSTRING(CALL_START_TIME,3,2),SUBSTRING(CALL_START_TIME,5,2)) as min_sec,
-  CONCAT_WS(':', SUBSTRING(CALL_START_TIME,0,2),SUBSTRING(CALL_START_TIME,3,2),SUBSTRING(CALL_START_TIME,5,2)) as start_call_time
+  CONCAT_WS(':', SUBSTRING(CALL_START_TIME,0,2),SUBSTRING(CALL_START_TIME,3,2),SUBSTRING(CALL_START_TIME,5,2)) as start_call_time,
+  CONCAT_WS('-', SUBSTRING(call_start_date,0,4),SUBSTRING(call_start_date,5,2),SUBSTRING(call_start_date,7,2)) as start_call_date,
+    CONCAT_WS(':', SUBSTRING(call_end_time,0,2),SUBSTRING(call_end_time,3,2),SUBSTRING(call_end_time,5,2)) as end_call_time,
+  CONCAT_WS('-', SUBSTRING(call_end_date,0,4),SUBSTRING(call_end_date,5,2),SUBSTRING(call_end_date,7,2)) as end_call_date,
+  FORMAT_DATE(CAST(CONCAT_WS('-', SUBSTRING(call_start_date,0,4),SUBSTRING(call_start_date,5,2),SUBSTRING(call_start_date,7,2)) as date), 'E') as weekly_range,
+  CONCAT_WS('-',CONCAT_WS(':', SUBSTRING(CALL_START_TIME,0,2),'00'),CONCAT_WS(':', SUBSTRING(CALL_START_TIME,0,2),'59')) as hourly_range
 FROM BIGINT_to_timestamp
 EMIT CHANGES;
 
--- 4th step am_pm_time
-DROP STREAM startdate;
-SET 'auto.offset.reset' = 'earliest';
-CREATE STREAM startdate AS 
-SELECT 
-  CONCAT_WS('-', SUBSTRING(call_start_date,0,4),SUBSTRING(call_start_date,5,2),SUBSTRING(call_start_date,7,2)) as start_call_date
-FROM BIGINT_to_timestamp
-EMIT CHANGES;
